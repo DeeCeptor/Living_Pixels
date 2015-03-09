2923f41eaa4f5dcb6ca5ec060cb315de2815d6fe
DeeCeptor <sirmichaellong@gmail.com>
2015-03-08 21:44:32 -0600

    Added RTS camera movement and zooming.

--- Living_Pixels_Unity/Assets/Scripts/CameraMovement.cs
+++ Living_Pixels_Unity/Assets/Scripts/CameraMovement.cs
@@ -9,8 +9,38 @@ public class CameraMovement : MonoBehaviour
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{	
+		float mousePosX = Input.mousePosition.x; 
+		float mousePosY = Input.mousePosition.y; 
+		int windowEdgeArea = 7; 
+		float scrollSpeed = 10;
+		
+		if (mousePosX < windowEdgeArea || Input.GetKey(KeyCode.LeftArrow))
+		{ 
+			transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime); 
+		} 
+		
+		if (mousePosX >= Screen.width - windowEdgeArea || Input.GetKey(KeyCode.RightArrow))
+		{ 
+			transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime); 
+		}
+		
+		if (mousePosY < windowEdgeArea || Input.GetKey(KeyCode.DownArrow))
+		{ 
+			transform.Translate(transform.up * -scrollSpeed * Time.deltaTime); 
+		} 
+		
+		if (mousePosY >= Screen.height - windowEdgeArea || Input.GetKey(KeyCode.UpArrow))
+		{ 
+			transform.Translate(transform.up * scrollSpeed * Time.deltaTime); 
+		}
+
+		// Scroll zooming
+		float scrollInput = Input.GetAxis ("Mouse ScrollWheel");
+		if (scrollInput != 0) 
+		{
+			camera.orthographicSize -= scrollInput * 3;
+		}
 	}
 }
